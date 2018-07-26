65ce64b5657838df6b555ac0e6efc3b364949b46
korlando7 <korlando7@gmail.com>
2018-07-25 23:21:40 -0400

    adding private and public toggling

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -10,60 +10,80 @@ const core = electron.remote.getGlobal('core')
 class ControlPanel extends Component {
   constructor () {
     super()
-    this.handleGoPublic = this.handleGoPublic.bind(this)
-    this.handleGoPrivate = this.handleGoPrivate.bind(this)
-    this.handleKeyPress = this.handleKeyPress.bind(this)
     this.handleRecipientsChange = this.handleRecipientsChange.bind(this)
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.handleButtonClick = this.handleButtonClick.bind(this)
     this.state = {
       rawRecipients: ''
     }
   }
-  handleGoPublic () {
-    this.props.togglePrivatePanel()
-    core.mode.setPublic()
-  }
-  handleGoPrivate () {
-    // need some recipients to go private with
-    // so allow them to enter that info
-    this.props.togglePrivatePanel()
-  }
+
   handleRecipientsChange (e) {
     this.setState({ rawRecipients: e.target.value })
   }
+
   handleKeyPress (e) {
     if (e.key === 'Enter') {
       // this is a little to text based so we'll have to make it more fun
       const recipients = this.state.rawRecipients
         .split(',')
         .map(x => x.trim())
+      this.setState({ rawRecipients: '' })
       core.commands.private(recipients)
-        .then(({ result }) => console.log(result))
+        .then(({ result }) => {
+          this.props.setPrivate()
+        })
         .catch((e) => console.log(e))
     }
   }
+
+  handleButtonClick (e) {
+    e.preventDefault()
+    this.props.setPublic()
+    core.commands.quit()
+      .then(({ result }) => console.log(result))
+      .catch((e) => console.log(e))
+  }
+
   render () {
     return (
       <div className='control-panel'>
-        <button onClick={this.handleGoPublic}>PUBLIC</button>
-        <button onClick={this.handleGoPrivate}>PRIVATE</button>
-        {this.state.goingPrivate &&
+        <div>
           <input
+            className='control-panel__input'
             type='text'
+            placeholder='Start private message...'
             onKeyPress={this.handleKeyPress}
             onChange={this.handleRecipientsChange}
+            value={this.state.rawRecipients}
           />
-        }
+        </div>
+        <div>
+          {this.props.private &&
+            <button className='button' onClick={this.handleButtonClick}>public</button>
+          }
+        </div>
+        <div className='control-panel__users'>
+          <h1>users</h1>
+        </div>
       </div>
     )
   }
 }
 
+const mapStateToProps = state => ({
+  private: state.private
+})
+
 const mapDispatchToProps = dispatch => ({
-  togglePrivatePanel: bindActionCreators(Actions.togglePrivatePanel, dispatch)
+  setPrivate: bindActionCreators(Actions.setPrivate, dispatch),
+  setPublic: bindActionCreators(Actions.setPublic, dispatch)
 })
 
 ControlPanel.propTypes = {
-  togglePrivatePanel: PropTypes.func.isRequired
+  setPrivate: PropTypes.func.isRequired,
+  setPublic: PropTypes.func.isRequired,
+  private: PropTypes.bool.isRequired
 }
 
-export default connect(null, mapDispatchToProps)(ControlPanel)
+export default connect(mapStateToProps, mapDispatchToProps)(ControlPanel)
