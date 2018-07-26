a19ae77aacb51a5ecd4e7fab2e49cb9c7e89c0a8
korlando7 <korlando7@gmail.com>
2018-07-25 23:20:56 -0400

    removing private panel

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,6 @@ import * as Actions from './store/actions'
 import ControlPanel from './components/ControlPanel'
 import Messages from './components/Messages'
 import MessageInput from './components/MessageInput'
-import PrivatePanel from './components/PrivatePanel'
 
 const core = electron.remote.getGlobal('core')
 
@@ -27,7 +26,6 @@ class App extends Component {
     return (
       <div className='main'>
         <ControlPanel />
-        <PrivatePanel />
         <div className='message-view'>
           <Messages />
           <MessageInput />
