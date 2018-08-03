22161f076e8a620164f0baa150b4d5468174e4e0
korlando7 <korlando7@gmail.com>
2018-08-03 15:06:09 -0400

    author drawer functionality / styling

--- src/components/AuthorDrawer.jsx
+++ src/components/AuthorDrawer.jsx
@@ -5,21 +5,57 @@ import PropTypes from 'prop-types'
 import * as Actions from '../store/actions'
 
 class AuthorDrawer extends Component {
+  constructor () {
+    super()
+
+    this.startPrivateMessage = this.startPrivateMessage.bind(this)
+  }
+
+  startPrivateMessage (e, author) {
+    e.preventDefault()
+    const recipient = [author]
+    this.props.goPrivate(recipient)
+    this.props.closeAuthorDrawer()
+  }
+
   render () {
+    const { authors, currentAuthorId } = this.props
+    const author = (authors[currentAuthorId] || {}).name || currentAuthorId
+    console.log(authors[currentAuthorId])
     return (
       <div className='author-drawer'>
-        <span onClick={this.props.closeAuthorDrawer}>X</span>
+        <span className='button-close' onClick={this.props.closeAuthorDrawer}>X</span>
+        <div>
+          <h1 className='author-drawer__header'>{author}</h1>
+        </div>
+        <div>
+          <button className='button button-block'>block</button>
+          <button className='button button-follow'>follow</button>
+        </div>
+        <div>
+          <button className='button button-private' onClick={(e) => this.startPrivateMessage(e, author)}>start private</button>
+        </div>
+
       </div>
     )
   }
 }
 
 AuthorDrawer.propTypes = {
-  closeAuthorDrawer: PropTypes.func.isRequired
+  closeAuthorDrawer: PropTypes.func.isRequired,
+  goPrivate: PropTypes.func.isRequired,
+  currentAuthorId: PropTypes.string.isRequired,
+  authors: PropTypes.objectOf(PropTypes.object).isRequired
 }
 
-const mapDispatchToProps = dispatch => ({
-  closeAuthorDrawer: bindActionCreators(Actions.closeAuthorDrawer, dispatch)
+const mapStateToProps = state => ({
+  currentAuthorId: state.currentAuthorId,
+  authors: state.authors
 })
 
-export default connect(null, mapDispatchToProps)(AuthorDrawer)
+const mapDispatchToProps = dispatch => ({
+  closeAuthorDrawer: bindActionCreators(Actions.closeAuthorDrawer, dispatch),
+  goPrivate: bindActionCreators(Actions.goPrivate, dispatch)
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AuthorDrawer)
