de3fec7b8642f1977152fe5f87e5b113f7394503
Peter Squicciarini <stripedpajamas273@gmail.com>
2018-08-10 15:18:44 -0400

    Add tab complete for names and emojis

--- src/components/messageInput.jsx
+++ src/components/messageInput.jsx
@@ -2,18 +2,31 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
 import { bindActionCreators } from 'redux'
+import tabComplete from '../helpers/tabComplete'
 import * as Actions from '../store/actions'
 
 class MessageInput extends Component {
   constructor (props) {
     super(props)
 
-    this.handleKeyPress = this.handleKeyPress.bind(this)
+    this.tabCompleter = null
+
+    this.handleKeyDown = this.handleKeyDown.bind(this)
     this.handleFocusMessageInput = this.handleFocusMessageInput.bind(this)
   }
 
-  handleKeyPress (e) {
+  handleKeyDown (e) {
     const msg = e.target.value
+    if (e.key !== 'Tab') {
+      this.tabCompleter = null
+    }
+    if (e.key === 'Tab') {
+      if (!this.tabCompleter) {
+        this.tabCompleter = tabComplete(msg)
+      }
+      this.messageInput.value = this.tabCompleter()
+      e.preventDefault()
+    }
     if (e.key === 'Enter') {
       this.props.sendMessage(msg)
       this.messageInput.value = ''
@@ -33,7 +46,7 @@ class MessageInput extends Component {
           className='messenger-input'
           type='text'
           placeholder={`Send ${mode} message`}
-          onKeyPress={this.handleKeyPress}
+          onKeyDown={this.handleKeyDown}
           onChange={this.handleChange}
           ref={el => { this.messageInput = el }}
         />
---
+++ src/helpers/tabComplete.js
@@ -0,0 +1,44 @@
+import emojis from 'node-emoji'
+import electron from 'electron'
+
+const core = electron.remote.getGlobal('core')
+
+const emojiList = Object.keys(emojis.emoji).map(e => `:${e}:`)
+
+const author = (partial) => core.authors.findMatches(partial)
+
+const emoji = (partial) => {
+  // finding emojis that start with the partial
+  // we want to show these as options first
+  const startEmojis = emojiList.filter(em => em.startsWith(partial))
+  // finding emojis that contain the partial
+  const partialEmojis = emojiList.filter(em => em.includes(partial.slice(1)))
+  // returning the emojis that start with the partial and then the emojis that contain the partial
+  return [...new Set(startEmojis.concat(partialEmojis))]
+}
+
+export default (line) => {
+  const split = line.split(' ')
+  let beginning = split.slice(0, split.length - 1).join(' ')
+  let lastWord = split[split.length - 1]
+  let matches = []
+
+  if (lastWord.indexOf(':') === 0) { // emoji
+    matches = emoji(lastWord)
+  } else {
+    // if they didn't include an @ symbol, put one on
+    if (lastWord.indexOf('@') !== 0) {
+      lastWord = `@${lastWord}`
+    }
+
+    matches = author(lastWord)
+  }
+
+  // keep a separation between what's being tab-completed and what came before
+  if (beginning) {
+    beginning = `${beginning} `
+  }
+
+  let idx = -1
+  return () => (matches.length && `${beginning}${matches[++idx % matches.length]}`) || line
+}
